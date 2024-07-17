4b5c3013ecea1ab27ab4295313d061de827a49f6
庄建国 <9340100@qq.com>
2024-07-17 10:13:12 +0000

    https://docs.aws.amazon.com/amplify/latest/userguide/deploy-express-server.html

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -7,6 +7,7 @@ mkdir -p ./.amplify-hosting/compute
 cp -r ./api ./.amplify-hosting/compute/default
 cp -r ./client ./.amplify-hosting/compute/client
 
-cp -r public ./.amplify-hosting/static
+cp -r ./client/public ./.amplify-hosting/static
+cp -r ./client/dist ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
