cdb295d451fdfd8b64df43372cd095f08fbe759f
baozhoutao <baozhoutao@hotoa.com>
2024-07-18 17:15:46 +0800

    工作路径调整

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,9 +4,11 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./api ./.amplify-hosting/compute/default
+mkdir -p ./.amplify-hosting/compute/default
+
+cp -r ./api ./.amplify-hosting/compute/default/api
 cp -r ./client ./.amplify-hosting/compute/client
-cp -r ./node_modules ./.amplify-hosting/compute/node_modules
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r ./client/public ./.amplify-hosting/static
 cp -r ./client/dist ./.amplify-hosting/static
